edb146abde952e4767cf7d88053b2ed9507445d1
rockefeller23 <rockefeller23@428222c8-ca59-45eb-83da-ed7bea2bd5fe>
2015-04-03 16:57:44 +0000

    template selection dialog [#22]

--- trunk/ContractManager/src/de/janrieke/contractmanager/gui/action/GenerateOdfDocument.java
+++ trunk/ContractManager/src/de/janrieke/contractmanager/gui/action/GenerateOdfDocument.java
@@ -21,6 +21,8 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,12 +30,18 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.rmi.RemoteException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
@@ -44,6 +52,8 @@ import org.w3c.dom.NodeList;
 import de.janrieke.contractmanager.ContractManagerPlugin;
 import de.janrieke.contractmanager.Settings;
 import de.janrieke.contractmanager.rmi.Contract;
+import de.janrieke.contractmanager.rmi.Contract.IntervalType;
+import de.janrieke.contractmanager.server.ContractImpl;
 import de.janrieke.contractmanager.server.SettingsUtil;
 import de.willuhn.jameica.gui.Action;
 import de.willuhn.jameica.gui.GUI;
@@ -56,8 +66,28 @@ import de.willuhn.util.ApplicationException;
 /**
  * Generic Action for "History back" ;).
  */
-public class GenerateCancelation implements Action {
+public class GenerateOdfDocument implements Action {
+	
+	public class TemplateListEntry {
+		private Path file;
+		private String title;
+		public TemplateListEntry(Path file, String title) {
+			this.file = file;
+			this.title = title;
+		}
+		public String getFileName() {
+			return getFile().getFileName().toString();
+		}
+		public String getTitle() {
+			return title;
+		}
+		public Path getFile() {
+			return file;
+		}
+	}
 
+	private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
+	
 	/**
 	 * @see de.willuhn.jameica.gui.Action#handleAction(java.lang.Object)
 	 */
@@ -73,7 +103,7 @@ public class GenerateCancelation implements Action {
 		Contract p = (Contract) context;
 		
 		// Which type of document should be generated?
-		final List<Path> templates = new ArrayList<>();
+		final List<TemplateListEntry> templates = new ArrayList<>();
 		String templateFolderString;
 		try {
 			templateFolderString = Settings.getTemplateFolder();
@@ -88,9 +118,47 @@ public class GenerateCancelation implements Action {
 				public FileVisitResult visitFile(Path path, BasicFileAttributes attributes)
 						throws IOException {
 					if (path.getFileName().toString().endsWith(".odt")) {
-						templates.add(path);
+						String title = getDocumentTitle(path);
+						templates.add(new TemplateListEntry(path, title));
 					}
 					return FileVisitResult.CONTINUE;
+				}
+
+				private String getDocumentTitle(Path path) {
+					ZipFile zipFile = null;
+					Scanner s = null;
+					try {
+						zipFile = new ZipFile(path.toFile());
+						ZipEntry meta = zipFile.getEntry("meta.xml");
+						if (meta == null)
+							return null;
+						InputStream inputStream = zipFile.getInputStream(meta);
+						if (inputStream == null)
+							return null;
+					    s = new Scanner(inputStream);
+					    s.useDelimiter("\\A");
+					    if (!s.hasNext())
+					    	return null;
+					    String fileContents = s.next();
+					    
+					    Pattern TITLE_PATTERN = Pattern.compile("<dc:title>(.*)</dc:title>");
+					    Matcher m = TITLE_PATTERN.matcher(fileContents);
+					    if (m.find())
+					    	return new String(m.group(1).getBytes(), UTF8_CHARSET);
+					    
+						return null;
+					} catch (IOException e) {
+						return null;
+					} finally {
+						try {
+							if (zipFile != null)
+								zipFile.close();
+							if (s != null)
+								s.close();
+						} catch (IOException e) {
+							// Ok
+						}
+					}
 				}
 
 				@Override
@@ -106,7 +174,7 @@ public class GenerateCancelation implements Action {
 			Logger.error("Template folder search failed for " + templateFolderString, e);
 		}
 
-		String templateBaseName;		
+		TemplateListEntry selectedTemplate;
 		if (templates.size() == 0) {
 			Logger.error("No *.odt templates found in template folder " + templateFolderString);
 			return;
@@ -115,32 +183,32 @@ public class GenerateCancelation implements Action {
 			templateSelectionDialog.setPanelText(Settings.i18n().tr("Please select the template to use."));
 			templateSelectionDialog.setSize(450, SWT.DEFAULT);
 			templateSelectionDialog.setTitle(Settings.i18n().tr("Select template"));
-			templateSelectionDialog.addColumn(Settings.i18n().tr("Template"), "fileName");
-			Object dialogResult = null;
+			templateSelectionDialog.addColumn(Settings.i18n().tr("Filename"), "fileName");
+			templateSelectionDialog.addColumn(Settings.i18n().tr("Title"), "title");
 			try {
-				dialogResult = templateSelectionDialog.open();
+				selectedTemplate = (TemplateListEntry) templateSelectionDialog.open();
 			} catch (Exception e) {
 				if (e instanceof OperationCanceledException)
 					return;
 				Logger.error("Failed to open template selection dialog", e);
-				dialogResult = templates.get(0);
+				selectedTemplate = templates.get(0);
 			}
 			
-			if (dialogResult == null)
+			if (selectedTemplate == null)
 				return;
-
-			templateBaseName = ((Path)dialogResult).getFileName().toString();
 		} else {
-			templateBaseName = templates.get(0).toString();
+			selectedTemplate = templates.get(0);
 		}
-		
+
+		// Get file name without extension to be used for generating the suggested document filename.
+		String templateBaseName = selectedTemplate.getFileName();
 		if (templateBaseName.contains(".")) {
 			templateBaseName = templateBaseName.substring(0, templateBaseName.lastIndexOf("."));
 		}
 		
-		// Select file name
+		// Select document filename
 		FileDialog fd = new FileDialog(GUI.getShell(), SWT.SAVE);
-		fd.setText(Settings.i18n().tr("Select Filename for Cancellation"));
+		fd.setText(Settings.i18n().tr("Select Filename for Document"));
 		fd.setOverwrite(true);
 
 		String contractName = "";
@@ -159,7 +227,7 @@ public class GenerateCancelation implements Action {
 				suggestedFilename.append('_');
 		}
 
-		fd.setFileName(Settings.i18n().tr(templateBaseName + "-{0}-{1}.odt",
+		fd.setFileName(MessageFormat.format(templateBaseName + "-{0}-{1}.odt",
 				Settings.dateformat(new Date()), suggestedFilename.toString()));
 
 		// String path = System.getProperty("user.home");
@@ -213,10 +281,21 @@ public class GenerateCancelation implements Action {
 				values.put("CANCELLATION_DATE",
 						Settings.dateformat(new Date()));
 
-			// open template document from plugin directory
-			FileInputStream fis = new FileInputStream(ContractManagerPlugin
-					.getInstance().getManifest().getPluginDir()
-					+ "/templates/Kuendigung.odt");
+			Date endDate = p.getEndDate();
+			if (endDate != null) {
+				// extend contract until the endDate + runtime
+				Calendar until = Calendar.getInstance();
+				until.setTime(endDate);
+				Integer followingMinRuntimeCount = p.getFollowingMinRuntimeCount();
+				IntervalType followingMinRuntimeType = p.getFollowingMinRuntimeType();
+				if (followingMinRuntimeCount != null && followingMinRuntimeType != null) {
+					ContractImpl.addToCalendar(until, followingMinRuntimeType, followingMinRuntimeCount);
+				}
+				values.put("UNTIL", Settings.dateformat(until.getTime()));
+			}
+
+			// open template document
+			FileInputStream fis = new FileInputStream(selectedTemplate.getFile().toFile());
 			OdfDocument doc = OdfDocument.loadDocument(fis);
 
 			// set the values of the variables
@@ -234,7 +313,7 @@ public class GenerateCancelation implements Action {
 			// save the file with a new file name
 			doc.save(filename);
 			GUI.getStatusBar().setSuccessText(
-					Settings.i18n().tr("Cancellation successfully generated."));
+					Settings.i18n().tr("Document successfully generated."));
 		} catch (Exception e) {
 			throw new ApplicationException(Settings.i18n().tr(
 					"Error while storing document"), e);
@@ -244,7 +323,7 @@ public class GenerateCancelation implements Action {
 			YesNoDialog d = new YesNoDialog(YesNoDialog.POSITION_CENTER);
 			d.setTitle(Settings.i18n().tr("Open file?"));
 			d.setText(Settings.i18n().tr(
-					"Would you like to open the generated cancellation file?"));
+					"Would you like to open the generated document?"));
 
 			Boolean choice;
 			try {
